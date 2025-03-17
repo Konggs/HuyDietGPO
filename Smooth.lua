c458128a9ea683a4ff31fb72e5479d36ee2ac269
Konggs <bao123.v5@gmail.com>
2025-03-17 12:10:07 +0700

    Update Smooth.lua

--- Smooth.lua
+++ Smooth.lua
@@ -96,18 +96,6 @@ task.delay(5, function()
             v.Enabled = false
         end
     end)
-    if localPlayer:FindFirstChild("PlayerGui") then
-         local playerGui = localPlayer:FindFirstChild("PlayerGui")
-         local guiNames = {
-             "BackpackGui","BossHP","HUD"
-         }
-         for _, name in ipairs(guiNames) do
-             local gui = playerGui:FindFirstChild(name)
-             if gui then
-                 gui.Enabled = false
-             end
-         end
-     end
     local Effects = RS:FindFirstChild("Effects")
     local HitEffect = Effects and Effects:FindFirstChild("HitEffect")
     if HitEffect then
